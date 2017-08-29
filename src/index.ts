4916a9fd1e590652aae2a013aed44634b10536ab
Rogerio Angeliski <rogerio.angeliski@bluesoft.com.br>
2017-08-29 16:16:00 -0300

    Iniciando estudo do webpack + typescript

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import * as express from "express";
+
+const app = express();
+
+app.set("port", process.env.PORT || 3000);
+
+app.get("*", (req, res)=>{
+  const versao = 1;
+  res.json({ versao });
+})
+
+app.listen(app.get("port"), () => {
+    console.log(("  App is running at http://localhost:%d in %s mode"), app.get("port"), app.get("env"));
+    console.log("  Press CTRL-C to stop\n");
+  });
+  
+  module.exports = app;
+  
