2243162e23322e48b00572c29287ce87f8f091f7
Rogerio Angeliski <rogerio.angeliski@bluesoft.com.br>
2017-08-30 13:59:17 -0300

    Teste de inicialização do banco via Promise

---
+++ src/database/index.ts
@@ -0,0 +1,7 @@
+export default  new Promise((resolve, reject) => {
+    console.info('Init Database module');
+
+    resolve({ 
+        
+    });
+})
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,29 @@
-import * as express from "express";
+import * as Promise from 'bluebird';
+import * as express from 'express';
+import database from './database';
+
+const startApp = (database: any) => {
+
+  app.get("*", (req, res)=>{
+    const versao = 1;
+    res.json({ versao });
+  })
+  
+  app.listen(app.get("port"), () => {
+      console.log(("  App is running at http://localhost:%d in %s mode"), app.get("port"), app.get("env"));
+      console.log("  Press CTRL-C to stop\n");
+    });
+      
+}
+
+database
+.then(startApp)
+.catch((error) => {
+  console.log('erro', error);
+});
 
 const app = express();
-
 app.set("port", process.env.PORT || 3000);
 
-app.get("*", (req, res)=>{
-  const versao = 1;
-  res.json({ versao });
-})
-
-app.listen(app.get("port"), () => {
-    console.log(("  App is running at http://localhost:%d in %s mode"), app.get("port"), app.get("env"));
-    console.log("  Press CTRL-C to stop\n");
-  });
-  
-  module.exports = app;
+module.exports = app;
   
